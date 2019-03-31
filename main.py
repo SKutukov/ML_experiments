ecd663f5b3750156d2c161b60f6122f1dcfba918
SKutukov <skutukov96@gmail.com>
2019-04-01 02:59:10 +0300

    minor fix

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ from sklearn.metrics import mean_squared_error
 
 from sklearn.utils import shuffle
 
+
 import sys
 
-# redirect output 
+# redirect output
 sys.stdout = open("out.txt", "w")
 
 # Where the downloaded data are
@@ -46,47 +47,39 @@ data['liked'] = data['feedback'].apply(feedback_to_float)
 def transform_data(data):
     users_data = data.groupby('instanceId_userId')
     resdata = None
-    for user_data in users_data:
-        user_data_liked = user_data[user_data['liked'] == 1]
-        user_data_disliked = user_data[user_data['liked'] == 0]
+    # for user_data in users_data:
+    user_data = users_data[0][0]
+    user_data_liked = user_data[user_data['liked'] == 1]
+    user_data_disliked = user_data[user_data['liked'] == 0]
+    user_data_liked = shuffle(user_data_liked)
+    user_data_disliked = shuffle(user_data_disliked)
+    if user_data_liked.shape[0] == 0 or user_data_disliked.shape[0] == 0:
+        continue
+    liked_mask  = np.random.randint(2, size=user_data_liked.shape[0]).astype(bool)
+    disliked_mask = np.random.randint(2, size=user_data_disliked.shape[0]).astype(bool)
+    liked_mask[0] = True
+    disliked_mask[0] = True
+    user_data_liked_masked = user_data_liked[liked_mask].values
+    user_data_disliked_masked = user_data_disliked[disliked_mask].values
+    data_disliked = user_data_disliked_masked[[
+        'instanceId_userId',
+        'auditweights_svd_prelaunch',
+        'auditweights_ctr_gender',
+        'auditweights_friendLikes'
+        ]].fillna(0.0).values
+    r = np.array((None, data.shape[1]))
+    for data_liked_masked in user_data_liked_masked[[
+        'instanceId_userId',
+        'auditweights_svd_prelaunch',
+        'auditweights_ctr_gender',
+        'auditweights_friendLikes'
+        ]].fillna(0.0).values:
 
-        user_data_liked = shuffle(user_data_liked)
-        user_data_disliked = shuffle(user_data_disliked)
-
-        if user_data_liked.shape[0] == 0 or user_data_disliked.shape[0] == 0:
-            continue
-
-        liked_mask  = np.random.randint(2, size=user_data_liked.shape[0]).astype(bool)
-        disliked_mask = np.random.randint(2, size=user_data_disliked.shape[0]).astype(bool)
-
-        liked_mask[0] = True
-        disliked_mask[0] = True
-
-        user_data_liked_masked = user_data_liked[liked_mask].values
-        user_data_disliked_masked = user_data_disliked[disliked_mask].values
-
-        data_disliked = user_data_disliked_masked[[
-            'instanceId_userId',
-            'auditweights_svd_prelaunch',
-            'auditweights_ctr_gender',
-            'auditweights_friendLikes'
-            ]].fillna(0.0).values
-
-        r = np.array((None, data.shape[1]))
-        for data_liked_masked in user_data_liked_masked[[
-            'instanceId_userId',
-            'auditweights_svd_prelaunch',
-            'auditweights_ctr_gender',
-            'auditweights_friendLikes'
-            ]].fillna(0.0).values:
-        
-            a = np.repeat(data_liked_masked, data_disliked.shape[0])
-            np.contaginate(r, np.concatenate(a, data_disliked, axis=1), axis=0)
-
-        return r , np.repeat(1, r.shape[0])
+        a = np.repeat(data_liked_masked, data_disliked.shape[0])
+        np.contaginate(r, np.concatenate(a, data_disliked, axis=1), axis=0)
+    return r , np.repeat(1, r.shape[0])
 
 print(transform_data(data))
-exec(0)         
 
 # Extract the most interesting features
 X = data[[
