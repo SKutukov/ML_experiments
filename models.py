24e3bbdb044b187a60e4ccd6e457691e5a48867d
SKutukov <skutukov96@gmail.com>
2019-05-04 01:05:49 +0300

    add network

--- models.py
+++ models.py
@@ -1,29 +1,90 @@
+import tensorflow as tf
 import numpy as np
 
 class Network:
-    def __init__(self):
-        self.model = Sequential()
-        self.model.add(Dense(4, input_dim=8, activation='relu'))
-        self.model.add(Dense(4, input_dim=4, activation='softmax'))
-        self.model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    def __init__(self, x_shape, y_shape, learning_rate, gamma=0.95, restore_path=None):
 
-    def fit(self, X, Y, epochs, batch_size):
-        self.model.fit(X, Y, epochs=epochs, batch_size=batch_size)
+        self.gamma = gamma
+        self.build_network(learning_rate=learning_rate, x_shape=x_shape, y_shape=y_shape)
+        self.sess = tf.Session()
+        tf.summary.FileWriter("logs/", self.sess.graph)
+        self.sess.run(tf.global_variables_initializer())
+        self.saver = tf.train.Saver()
+        if restore_path is not None:
+            self.saver.restore(self.sess, restore_path)
 
-    def predict(self, env, s):
-        return self.model.predict(s)
 
-class Table:
-    def __init__(self):
-        self.table = np.random.rand(4, 8)
-        self.alpha = 0.05
-        self.gamma = 0.5
-  
-    def fit(self, s_0, s_1, reward, a):
-        
-        self.table = self.table @ np.array(s_0)[a] + self.alpha * (reward + self.gamma *
-             np.argmax(self.table @ np.array(s_1)))         
+    def predict(self, observation):
+        observation = observation[:, np.newaxis]
+        prob_weights = self.sess.run(self.outputs_softmax, feed_dict={self.X: observation})
+        action = np.random.choice(range(len(prob_weights.ravel())), p=prob_weights.ravel())
+        return action
 
-    def predict(self, env, s):
-        print(s)
-        return np.argmax(self.table @ np.array(s))
+    def fit(self, episode_observations, episode_actions, episode_rewards):
+
+        self.sess.run(self.train_op, feed_dict={
+             self.X: np.vstack(episode_observations).T,
+             self.Y: np.vstack(np.array(episode_actions)).T,
+             self.epoch_rewards: self.calc_reward(episode_rewards),
+        })
+
+
+    def save_model(self, save_path):
+        self.saver.save(self.sess, save_path)
+
+    def calc_reward(self, episode_rewards):
+        discounted_r = np.zeros_like(episode_rewards)
+        running_add = 0
+        for t in reversed(range(len(episode_rewards))):
+            running_add = running_add * self.gamma + episode_rewards[t]
+            discounted_r[t] = running_add
+        return discounted_r
+
+
+    def build_network(self, x_shape, y_shape, learning_rate=0.01):
+        with tf.name_scope('inputs'):
+            self.X = tf.placeholder(tf.float32, shape=(x_shape, None), name="X")
+            self.Y = tf.placeholder(tf.float32, shape=(y_shape, None), name="Y")
+            self.epoch_rewards = tf.placeholder(tf.float32, [None, ], name="rewards")
+
+        units_layers = [10, 10, 10]
+
+        with tf.name_scope('layer_1'):
+            W1 = tf.get_variable("W1", [units_layers[0], x_shape],
+                                 initializer=tf.contrib.layers.xavier_initializer(seed=1))
+            b1 = tf.get_variable("b1", [units_layers[0], 1], initializer=tf.contrib.layers.xavier_initializer(seed=1))
+            Z1 = tf.add(tf.matmul(W1, self.X), b1)
+            A1 = tf.nn.relu(Z1)
+
+        with tf.name_scope('layer_2'):
+            W2 = tf.get_variable("W2", [units_layers[1], units_layers[0]],
+                                 initializer=tf.contrib.layers.xavier_initializer(seed=1))
+            b2 = tf.get_variable("b2", [units_layers[1], 1], initializer=tf.contrib.layers.xavier_initializer(seed=1))
+            Z2 = tf.add(tf.matmul(W2, A1), b2)
+            A2 = tf.nn.relu(Z2)
+
+        with tf.name_scope('layer_3'):
+            W3 = tf.get_variable("W3", [units_layers[2], units_layers[1]],
+                                 initializer=tf.contrib.layers.xavier_initializer(seed=1))
+            b3 = tf.get_variable("b3", [units_layers[2], 1], initializer=tf.contrib.layers.xavier_initializer(seed=1))
+            Z3 = tf.add(tf.matmul(W3, A2), b3)
+            A3 = tf.nn.relu(Z3)
+
+        with tf.name_scope('layer_4'):
+            W4 = tf.get_variable("W4", [y_shape, units_layers[2]],
+                                 initializer=tf.contrib.layers.xavier_initializer(seed=1))
+            b4 = tf.get_variable("b4", [y_shape, 1], initializer=tf.contrib.layers.xavier_initializer(seed=1))
+            Z4 = tf.add(tf.matmul(W4, A3), b4)
+            A4 = tf.nn.softmax(Z4)
+
+        logits = tf.transpose(Z4)
+        labels = tf.transpose(self.Y)
+        self.outputs_softmax = tf.nn.softmax(logits, name='A4')
+
+        with tf.name_scope('loss'):
+            neg_log_prob = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels)
+            loss = tf.reduce_mean(neg_log_prob * self.epoch_rewards)
+
+        with tf.name_scope('train'):
+            self.train_op = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+
---
+++ train.py
@@ -0,0 +1,83 @@
+from gym.envs.box2d.lunar_lander import LunarLander
+import numpy as np
+from models import Network
+import time
+if __name__ == "__main__":
+
+    is_render = True
+    current_epoch = 0
+    epochs_count = 5000
+    rewards = []
+    reward_min = 5000
+
+    env = LunarLander()
+
+    load_version = 0
+    training_version = 1
+    restore_path = "res/weights/%s/LunarLander-v2.ckpt".format(str(training_version))
+    save_path = "res/weights/%s/LunarLander-v2.ckpt".format(str(training_version))
+
+    min_reward = -500
+    time_limit = 60
+
+    model = Network(x_shape=env.observation_space.shape[0],
+                    y_shape=env.action_space.n,
+                    learning_rate=0.02,
+                    gamma=0.99,
+                    restore_path=None)
+
+
+    for epoch in range(current_epoch, epochs_count):
+
+        state = env.reset()
+        episode_reward = 0
+        epoche_observations = []
+        epoche_actions = []
+        epoche_rewards = []
+        time_begin = time.clock()
+
+        while True:
+            if is_render:
+                env.render()
+
+            action = model.predict(state)
+
+            state_, reward, done, info = env.step(action)
+
+            epoche_observations.append(state)
+            epoche_rewards.append(reward)
+
+            action_onehot = np.zeros(env.action_space.n)
+            action_onehot[action] = 1
+
+            epoche_actions.append(action_onehot)
+
+            episode_rewards_sum = sum(epoche_rewards)
+            if episode_rewards_sum < min_reward:
+                done = True
+
+            if time.clock() - time_begin > time_limit:
+                done = True
+
+            if done:
+                episode_rewards_sum = sum(epoche_rewards)
+                rewards.append(episode_rewards_sum)
+                max_reward_so_far = np.amax(rewards)
+
+                print("-----------------------")
+                print("Episode: ", epoch)
+                print("Reward: ", episode_rewards_sum)
+                print("Max reward so far: ", max_reward_so_far)
+                print("-----------------------")
+
+                model.fit(episode_actions=epoche_actions, episode_rewards=epoche_rewards,
+                                                          episode_observations=epoche_observations)
+                epoche_observations = []
+                epoche_actions = []
+                epoche_rewards = []
+
+                model.save_model(save_path)
+                break
+
+            # Save new observation
+            state = state_
