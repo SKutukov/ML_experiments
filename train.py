f9f0862909e028706cd0c1f17e29fad1c6b961f5
skutukov <skutukov@SKUTUKOV.croc.ru>
2019-05-20 20:29:14 +0300

    one more try

--- train.py
+++ train.py
@@ -5,14 +5,14 @@ import time
 from dataset import ReplayBuffer
 if __name__ == "__main__":
 
-    is_render = False 
+    is_render = True 
     current_epoch = 0
     epochs_count = 10000
     max_reward = -200
     env = LunarLander()
 
-    load_version = 2
-    training_version = 3
+    load_version = 3
+    training_version = 4
     if load_version != 0:
         restore_path = "res/last_weight/{}/LunarLander-v2.ckpt".format(load_version)
     else:
@@ -21,12 +21,12 @@ if __name__ == "__main__":
     save_path = "res/last_weight/{}/LunarLander-v2.ckpt".format(training_version)
 
     min_reward = -200
-    time_limit = 15
+    time_limit = 5
 
     model = Network(x_shape=env.observation_space.shape[0],
                     y_shape=env.action_space.n,
                     learning_rate=0.00002,
-                    gamma=0.5,
+                    gamma=0.95,
                     restore_path=restore_path)
 
     replBuffer = ReplayBuffer()
@@ -51,6 +51,9 @@ if __name__ == "__main__":
 
             if time.clock() - time_begin > time_limit:
                 action = 0
+
+            #if  state[1] < 0.01:
+            #    action = 0
 
             state_, reward, done, info = env.step(action)
 
